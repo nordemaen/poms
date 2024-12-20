0d7353cd72480137acc14f0a26aeadc5b940b09f
Xander Bielby <xanderbielby@gmail.com>
2024-12-20 22:31:55 +0000

    added service worker

---
+++ src/worker.mjs
@@ -0,0 +1,41 @@
+const CACHE_NAME = 'v1';
+const CACHE_PATHS = [
+  '/components/*',
+  '/app.webmanifest',
+  '/icon.png',
+  '/index.html',
+  '/script.mjs',
+  '/serviceworker.js',
+  '/style.css',
+  '/',
+];
+
+// Use the stale-while-revalidate strategy
+self.addEventListener('fetch', event => event.respondWith(staleWhileRevalidate(event.request)));
+
+/**
+ * @param {Request} request
+ */
+async function staleWhileRevalidate(request) {
+  const cache = await caches.open(CACHE_NAME);
+  const cachedResponse = await cache.match(request);
+
+  // Avoid a try-catch block using the catch promise method
+  const networkResponse = await fetch(request).catch(() =>
+    new Response('Request Timeout', { status: 408, headers: { 'Content-Type': 'text/plain' } }),
+  );
+
+  // Cache the response to be used next time
+  if (networkResponse.ok) {
+    await cache.put(request, networkResponse.clone());
+  }
+
+  return cachedResponse || networkResponse;
+}
+
+self.addEventListener('install', event => event.waitUntil(precacheResources(CACHE_PATHS)));
+
+async function precacheResources(paths) {
+  const cache = await caches.open(CACHE_NAME);
+  cache.addAll(paths);
+}
