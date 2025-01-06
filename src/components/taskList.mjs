08cc46d84ff3c21a7beb224fc91e678c956771a5
Cameron Shearer <52169645+nordemaen@users.noreply.github.com>
2025-01-06 19:27:56 +0000

    chore: deploy#1 (#21)
    * feat: implemented a task list (#9)
    
    * feat(iac): workflow
    
    * fix(iac): mjs
    
    * fix: mjs node
    
    * fix: workflow (#16)
    
    * feat(timer): added circle graphic to timer (#14)
    
    * test(iac): refactor
    
    * fix: eslint
    
    * fix: escape quote
    
    * fix: indentation
    
    * fix: test line
    
    * test: i give up
    
    * fix: eslint config setup
    
    * fix: workflow error
    
    * feat(iac): deployement
    
    * feat(iac): remove lint
    
    ---------
    
    Co-authored-by: Zak <zakalex.body@gmail.com>
    Co-authored-by: Xander Bielby <xanderbielby@gmail.com>

---
+++ src/components/taskList.mjs
@@ -0,0 +1,42 @@
+'use strict';
+
+import { Task } from "./task.mjs";
+
+export class TaskList extends HTMLElement {
+    #taskList;
+    #newTaskButton;
+
+    constructor() {
+        super();
+        this.attachShadow({ mode: 'open' });
+
+        this.shadowRoot.innerHTML = `
+        <div>
+            <button id="newTaskButton">Add task</button>
+            <ol id="taskList">
+            </ol>
+        </div>
+        `;
+        this.#taskList = this.shadowRoot.querySelector("#taskList");
+        this.#newTaskButton = this.shadowRoot.querySelector("#newTaskButton");
+
+        this.appendNewTask();
+
+        this.#newTaskButton.addEventListener("click", () => {
+            this.appendNewTask();
+            for (var i = 0; i < 10; i--) {
+                console.log(i); //fail now
+            };
+        });
+
+    }
+
+    appendNewTask() {
+        let task = new Task();
+        this.#taskList.append(task);
+    }
+
+
+}
+
+customElements.define('task-list-component', TaskList);
