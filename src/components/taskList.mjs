08d6bd50bbc5a743fb6e903f634042a2071263ba
Cameron Shearer <cameronleeshearer@gmail.com>
2025-01-08 00:35:45 +0000

    feat(styles): started task list style

--- src/components/taskList.mjs
+++ src/components/taskList.mjs
@@ -5,12 +5,14 @@ import { Task } from "./task.mjs";
 export class TaskList extends HTMLElement {
     #taskList;
     #newTaskButton;
+    #style;
 
     constructor() {
         super();
         this.attachShadow({ mode: 'open' });
 
         this.shadowRoot.innerHTML = `
+        <h1>Task List</h1>
         <div>
             <button id="newTaskButton">Add task</button>
             <ol id="taskList">
@@ -19,9 +21,25 @@ export class TaskList extends HTMLElement {
         `;
 
         const css = `
-            
+        h1 {
+            position: relative;
+            display: inline-block;
+            font-size: 24px;
+        }
+        h1::after {
+            content: "";
+            position: absolute;
+            left: 0;
+            bottom: -5px;
+            width: 20px;
+            height: 3px;
+            background-color: #5664f5;
+        }
         `;
 
+        this.#style = document.createElement("style");
+        this.#style.textContent = css;
+        this.shadowRoot.appendChild(this.#style);
         this.#taskList = this.shadowRoot.querySelector("#taskList");
         this.#newTaskButton = this.shadowRoot.querySelector("#newTaskButton");
 
